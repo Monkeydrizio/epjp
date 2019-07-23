1f3ed4fd9aeee11c1e9f69d4d4a86d6d7addbd61
Monkeydrizio <maurizio.pilato90@gmail.com>
2019-07-23 14:55:53 +0200

    if else exercises

--- src/b04/s38/IfElse.java
+++ src/b04/s38/IfElse.java
@@ -2,14 +2,21 @@ package b04.s38;
 
 public class IfElse {
     public static void main(String[] args) {
-        boolean condition = false;
+               
+        int value = 100;
+                       
+        if (value <=100 && value>=0) {
         
-        if (condition) {
-            System.out.println("only if condition is true");
+        if (value %2==0) {
+            System.out.println(value + " è un numero pari");
         } else {
-            System.out.println("only if condition is false");
+            System.out.println(value + " è un numero dispari");
         }
-
-        System.out.println("always printed");
+        }
+         else {
+        	System.out.println("il numero è troppo grande");
+        }
+        System.out.println("Cordiali saluti!"); 
     }
+      
 }
--- src/b04/s38/IfElseIfElse.java
+++ src/b04/s38/IfElseIfElse.java
@@ -2,15 +2,16 @@ package b04.s38;
 
 public class IfElseIfElse {
     public static void main(String[] args) {
-        boolean condition = false;
-        boolean otherCondition = true;
+    	int anno = 2020;
+        boolean condition = anno<2000;
+        boolean otherCondition = anno>2000 && anno<2100;
 
         if (condition) {
-            System.out.println("only if condition is true");
+            System.out.println("siamo nel secolo scorso");
         } else if (otherCondition) {
-            System.out.println("only if condition is false and otherCondition is true");
+            System.out.println("siamo nel 21esimo secolo");
         } else {
-            System.out.println("condition and otherCondition are both false");
+            System.out.println("benvenuto nel futuro");
         }
 
         System.out.println("always printed");
