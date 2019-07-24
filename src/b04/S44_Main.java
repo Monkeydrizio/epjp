5c3a8774d0f098d4d21dfc6abbb98ee667c6bd00
Monkeydrizio <maurizio.pilato90@gmail.com>
2019-07-24 12:59:02 +0200

    forza giovane

--- src/b04/S44_Main.java
+++ src/b04/S44_Main.java
@@ -7,9 +7,10 @@ public class S44_Main {
 		S44_chicken c1 = new S44_chicken(s);
 		S44_chicken c2 = new S44_chicken();
 		S44_chicken c3 = new S44_chicken("Genoveffa", 497.12);
-		S44_chicken c4 = new S44_chicken("Enrico", 1000, false);    // Il galletto
-		
-		System.out.println(c3 + "\n" + c4);    // "\n" va a capo, la stringa in mezzo trasforma i due oggetti in stringhe da stampare, in caso constrario si può usare il .toString()
-				
+		S44_chicken c4 = new S44_chicken("Enrico", 1000, false); // Il galletto
+
+		System.out.println(c3 + "\n" + c4); // "\n" va a capo, la stringa in mezzo trasforma i due oggetti in stringhe
+											// da stampare, in caso constrario si può usare il .toString()
+
 	}
 }
