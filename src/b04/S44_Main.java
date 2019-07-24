5c0999efcb166881bfed146cb50216276003db25
Monkeydrizio <maurizio.pilato90@gmail.com>
2019-07-24 10:51:19 +0200

    creazione polli

---
+++ src/b04/S44_Main.java
@@ -0,0 +1,15 @@
+package b04;
+
+public class S44_Main {
+	public static void main(String[] args) {
+		String s = "giustino";
+
+		S44_chicken c1 = new S44_chicken(s);
+		S44_chicken c2 = new S44_chicken();
+		S44_chicken c3 = new S44_chicken("Genoveffa", 497.12);
+		S44_chicken c4 = new S44_chicken("Enrico", 1000, false);    // Il galletto
+		
+		System.out.println(c3.toString() + "\n" + c4);
+				
+	}
+}
---
+++ src/b04/S44_chicken.java
@@ -0,0 +1,45 @@
+package b04;
+
+public class S44_chicken {
+	
+	static final double DEFAULT_WEIGHT = 1.0;
+	static final String DEFAULT_NAME = "Unknown";
+	static final boolean DEFAULT_FEMALE = true;
+
+	String name;
+	double weight;
+	boolean female;
+
+	public S44_chicken(String name) { // costruttore solo con nome e peso default
+
+		this.name = name;
+		this.weight = DEFAULT_WEIGHT;
+		this.female = DEFAULT_FEMALE;
+	}
+
+	public S44_chicken(String name, double weight) { // costruttore peso e nome
+
+		this.name = name;
+		this.weight = weight;
+		this.female = DEFAULT_FEMALE;
+	}
+
+	
+	public S44_chicken(String name, double weight, boolean female) { // costruttore peso, nome e sesso
+
+		this.name = name;
+		this.weight = weight;
+		this.female = female;
+	}
+	
+	public S44_chicken() { // costruttore di default
+		this.name = DEFAULT_NAME;
+		this.weight = DEFAULT_WEIGHT;
+		this.female = DEFAULT_FEMALE;
+	}
+
+	@Override
+	public String toString() {
+		return "S44_chicken [name=" + name + ", weight=" + weight + ", female=" + female + "]";
+	}
+}
