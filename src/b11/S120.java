beb5a77b00bcf004543dbba8d4326f3b0b1a5326
Administrator <Administrator@192.168.12.53>
2019-08-08 18:00:44 +0200

    esempio oggetto coder

--- src/b11/S120.java
+++ src/b11/S120.java
@@ -10,43 +10,59 @@ import java.util.List;
 import oracle.jdbc.pool.OracleDataSource;
 
 public class S120 {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-    private static final String USER = "me";
-    private static final String PASSWORD = "password";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe";
+	private static final String USER = "me";
+	private static final String PASSWORD = "password";
 
-    private OracleDataSource ods;
+	private OracleDataSource ods;
 
-    public S120() throws SQLException {
-        ods = new OracleDataSource();
+	public S120() throws SQLException {
+		ods = new OracleDataSource();
 
-        ods.setURL(URL);
-        ods.setUser(USER);
-        ods.setPassword(PASSWORD);
-    }
+		ods.setURL(URL);
+		ods.setUser(USER);
+		ods.setPassword(PASSWORD);
+	}
 
-    public List<String> getCoderNames() throws SQLException {
-        try (Connection conn = ods.getConnection(); //
-                Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery("SELECT first_name FROM coders ORDER BY 1");
+	public List<Coder> getCoderNames() throws SQLException {
+		try (Connection conn = ods.getConnection(); //
+				Statement stmt = conn.createStatement()) {
+			ResultSet rs = stmt.executeQuery("SELECT first_name, last_name, salary FROM coders ORDER BY 1");
 
-            List<String> results = new ArrayList<String>();
-            while (rs.next()) {
-                results.add(rs.getString(1));
-            }
-            return results;
-        }
-    }
+			List<Coder> results = new ArrayList<Coder>();
+			while (rs.next()) {
+				Coder coder = new Coder();
+				coder.fn = rs.getString(1);
+				coder.ln = rs.getString(2);
+				coder.salary = rs.getInt(3);
+				results.add(coder);
+			}
+			return results;
+		}
+	}
 
-    public static void main(String[] args) {
-        try {
-            S120 sample = new S120();
-            List<String> names = sample.getCoderNames();
+	public static void main(String[] args) {
+		try {
+			S120 sample = new S120();
+			List<Coder> names = sample.getCoderNames();
 
-            System.out.println("Coder names are: " + names);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return;
-        }
+			System.out.println("Coder names are: " + names);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return;
+		}
 
-    }
+	}
+}
+
+class Coder {
+	public String fn;
+	public String ln;
+	public int salary;
+
+	@Override
+	public String toString() {
+		return "[fn=" + fn + ", ln=" + ln + ", salary=" + salary + "]";
+	}
+
 }
